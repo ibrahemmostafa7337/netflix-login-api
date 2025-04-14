0febcb6516433163223ecc4b5417e95de63eac52
ibrahem mostafa <ibrahemmostafa7337@gmail.com>
2025-04-14 06:52:28 +0200

    first push

--- server.py
+++ server.py
@@ -38,5 +38,6 @@ def get_users():
                     continue
     return jsonify(users), 200
 
-if __name__ == '__main__':
-    app.run(debug=True)
+ if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
+
