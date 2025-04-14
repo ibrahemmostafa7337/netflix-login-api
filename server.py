ffe05f85f7c6d33372c3d7223f704ae6c174cb68
ibrahemmostafa7337 <ibrahemmostafa7337@gmail.com>
2025-04-14 08:37:49 +0200

    Add files via upload

--- server.py
+++ server.py
@@ -38,6 +38,7 @@ def get_users():
                     continue
     return jsonify(users), 200
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    from os import environ
+    app.run(host='0.0.0.0', port=int(environ.get('PORT', 5000)))
 
 
